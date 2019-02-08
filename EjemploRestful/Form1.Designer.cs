a4472ba6492b98915655cd7ca9dc6738cbe516fb
Mario Cascante Sanchez <mario.cascante@outlook.com>
2019-02-08 10:53:55 -0600

    Agregar archivos de proyecto.

---
+++ EjemploRestful/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace EjemploRestful
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComboProvincia = new System.Windows.Forms.ComboBox();
+            this.ComboCanton = new System.Windows.Forms.ComboBox();
+            this.ComboDistrito = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ComboProvincia
+            // 
+            this.ComboProvincia.FormattingEnabled = true;
+            this.ComboProvincia.Location = new System.Drawing.Point(34, 89);
+            this.ComboProvincia.Name = "ComboProvincia";
+            this.ComboProvincia.Size = new System.Drawing.Size(242, 21);
+            this.ComboProvincia.TabIndex = 0;
+            this.ComboProvincia.SelectedIndexChanged += new System.EventHandler(this.ComboProvincia_SelectedIndexChanged);
+            // 
+            // ComboCanton
+            // 
+            this.ComboCanton.FormattingEnabled = true;
+            this.ComboCanton.Location = new System.Drawing.Point(34, 142);
+            this.ComboCanton.Name = "ComboCanton";
+            this.ComboCanton.Size = new System.Drawing.Size(242, 21);
+            this.ComboCanton.TabIndex = 1;
+            this.ComboCanton.SelectedIndexChanged += new System.EventHandler(this.ComboCanton_SelectedIndexChanged);
+            // 
+            // ComboDistrito
+            // 
+            this.ComboDistrito.FormattingEnabled = true;
+            this.ComboDistrito.Location = new System.Drawing.Point(34, 196);
+            this.ComboDistrito.Name = "ComboDistrito";
+            this.ComboDistrito.Size = new System.Drawing.Size(242, 21);
+            this.ComboDistrito.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Provincia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Canton";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Distrito";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(299, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ejemplo de uso de RestFul";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 243);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ComboDistrito);
+            this.Controls.Add(this.ComboCanton);
+            this.Controls.Add(this.ComboProvincia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboProvincia;
+        private System.Windows.Forms.ComboBox ComboCanton;
+        private System.Windows.Forms.ComboBox ComboDistrito;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EjemploRestful/Form1.cs
@@ -0,0 +1,209 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploRestful
+{
+    public partial class Form1 : Form
+    {
+        
+       
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            ObtenerProvincias();
+          
+        }
+
+
+        public DataTable CrearDataSource(dynamic dynObj) {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("nombre");
+            //JContainer is the base class
+            var jObj = (JObject)dynObj;
+            foreach (JToken token in jObj.Children())
+            {
+                if (token is JProperty)
+                {
+                    var prop = token as JProperty;
+                    DataRow P = dt.NewRow();
+                    P["id"] = prop.Name;
+                    P["nombre"] = prop.Value;
+                    dt.Rows.Add(P);
+
+                }
+            }
+
+            return dt;
+        }
+
+
+        private void ObtenerProvincias() {
+
+            var request = (HttpWebRequest)WebRequest.Create("https://ubicaciones.paginasweb.cr/provincias.json");
+
+            request.Method = "GET";
+           
+
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            string content = string.Empty;
+
+            using (var stream = response.GetResponseStream())
+
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                }
+
+                dynamic dynObj = JsonConvert.DeserializeObject(content);                
+
+                        
+                ComboProvincia.DataSource=CrearDataSource(dynObj);
+                ComboProvincia.ValueMember = "id";
+                ComboProvincia.DisplayMember = "nombre";
+                ComboProvincia.Refresh();
+
+
+
+            }
+
+        }
+
+
+        private void ObtenerCatones(string idprovincia)
+        {
+            string url = "https://ubicaciones.paginasweb.cr/provincia/" + idprovincia + "/cantones.json";
+            var request = (HttpWebRequest)WebRequest.Create("https://ubicaciones.paginasweb.cr/provincia/"+idprovincia+"/cantones.json");
+
+            request.Method = "GET";
+
+
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                string content = string.Empty;
+
+                using (var stream = response.GetResponseStream())
+
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+
+                    dynamic dynObj = JsonConvert.DeserializeObject(content);
+
+
+                    ComboCanton.DataSource = CrearDataSource(dynObj);
+                    ComboCanton.ValueMember = "id";
+                    ComboCanton.DisplayMember = "nombre";
+                    ComboCanton.Refresh();
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+               
+
+
+            }
+
+           
+
+          
+
+        }
+
+        private void ObtenerDistritos(string idprovincia,string idcanton)
+        {
+
+            var request = (HttpWebRequest)WebRequest.Create("https://ubicaciones.paginasweb.cr/provincia/"+idprovincia+"/canton/"+idcanton+"/distritos.json");
+
+            request.Method = "GET";
+
+
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+
+            string content = string.Empty;
+
+            using (var stream = response.GetResponseStream())
+
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                }
+
+                dynamic dynObj = JsonConvert.DeserializeObject(content);
+
+
+                ComboDistrito.DataSource = CrearDataSource(dynObj);
+                ComboDistrito.ValueMember = "id";
+                ComboDistrito.DisplayMember = "nombre";
+                ComboDistrito.Refresh();
+
+
+
+            }
+
+            }
+            catch (Exception ex)
+            {
+
+
+
+            }
+
+        }
+
+        private void ComboCanton_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ObtenerDistritos(ComboProvincia.SelectedValue.ToString(), ComboCanton.SelectedValue.ToString());
+        }
+
+        private void ComboProvincia_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            if (!ComboProvincia.SelectedValue.ToString().Equals("System.Data.DataRowView"))
+                    {
+                ObtenerCatones(ComboProvincia.SelectedValue.ToString());
+                     }
+               
+        }
+
+        
+    }
+}
